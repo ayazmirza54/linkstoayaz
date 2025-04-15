52050d0a9857b8307eccb04fd118fcd048f4b484
Ayaz Mirza <37616943+ayazmirza54@users.noreply.github.com>
2025-04-15 06:37:23 +0000

    doneee

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { inject } from '@vercel/analytics';
+import { Analytics } from '@vercel/analytics/react';
  
 inject();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <><App />
+    <Analytics /></>
   </StrictMode>
 );
 
