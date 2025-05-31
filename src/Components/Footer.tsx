70813cb64b0493be737652fb833b3b52f9135fac
Ayaz Mirza <37616943+ayazmirza54@users.noreply.github.com>
2025-05-31 12:21:36 +0530

    Update Footer.tsx

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-const Footer = () => { return ( <footer className="w-full text-center py-4 border-t text-sm text-white">
-  <p className = ""> Built with <span className="text-red-500">❤️</span> and 
-    <span className="text-orange-500">⚡</span> by <strong>Ayaz</strong> </p> 
-</footer> ); };
+const Footer = () => { return ( <footer className="relative p-[2px] rounded-lg ">
+            <p className="bg-black rounded-lg p-4 h-full hover:bg-black/90 transition-all">
+                <div className="footer flex flex-col sm:flex-row justify-center items-center gap-2 text-sm">
+                    Built with <p className="text-red-500 inline">❤️</p> and <p className="text-orange-500 inline">⚡</p> by Ayaz
+                </div>
+            </p>
+        </footer> ); };
 
 export default Footer;
