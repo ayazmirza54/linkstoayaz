136d56d87833edd6878792771287bda7b07d6a18
Ayaz Mirza <37616943+ayazmirza54@users.noreply.github.com>
2025-05-31 12:13:33 +0530

    Create Footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Footer = () => { return ( <footer className="w-full text-center py-4 border-t text-sm text-gray-600"> <p className = "shimmer-text"> Built with <span className="text-red-500">❤️</span> and <span className="text-orange-500">⚡</span> by <strong>Ayaz</strong> </p> </footer> ); };
+
+export default Footer;
